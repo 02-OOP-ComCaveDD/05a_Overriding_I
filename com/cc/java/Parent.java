5f52e28b5ddb0ea2b0a689cbd3e761841c2690f7
Peter <schmidt.multimedia@gmail.com>
2024-05-27 12:21:34 +0200

    01 / Serielle Vererbung / Overriding

---
+++ com/cc/java/Parent.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Parent extends GrandParent{
+
+    public String sayHello(){
+        return "Hello from parent!";
+    }
+
+    public String sayHello(String str){
+        return "Hello, " + str + "!";
+    }
+
+
+
+}
